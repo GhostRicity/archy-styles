bfe36fc9221f3fdc9a49a9c215495a19bb7793be
Eduards Birznieks <eduards.latvia@gmail.com>
2020-05-18 19:08:52 +0200

    initial commit

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "Archy Styles",
+  shortName: "AS",
+  description: "Gerys personal blog",
+  url: "https://ArchyStyles.com", // Domain of your site without prefix!
+  prefix: "/",
+  author: "Eduards Birnzieks",
+  twitter: "",
+}
---
+++ src/pages/about.js
@@ -0,0 +1,75 @@
+import React from "react"
+import { graphql, StaticQuery } from "gatsby"
+import Img from "gatsby-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import "../utils/normalize.css"
+import "../utils/css/screen.css"
+
+const AboutPage = ({ data }, location) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout title={siteTitle}>
+      <SEO title="About" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
+
+      <article className="post-content page-template no-image">
+        <div className="post-content-body">
+          <h2 id="clean-minimal-and-deeply-customisable-london-is-a-theme-made-for-people-who-appreciate-simple-lines-">
+            Retro back car catalog
+          </h2>
+          <figure className="kg-card kg-image-card kg-width-full">
+            <Img
+              fluid={data.benchAccounting.childImageSharp.fluid}
+              className="kg-image"
+            />
+          </figure>
+          <h3 id="dynamic-styles">Old new new</h3>
+          <p>Look at cars hear read news and buy buy buy.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p>
+            Dont forget to check out the{" "}
+            <a href="https://eddythedev.com/">my portfolio website.</a> for
+            buissnes inquaries.
+          </p>
+        </div>
+      </article>
+    </Layout>
+  )
+}
+
+const indexQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    benchAccounting: file(relativePath: { eq: "darkg.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1360) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+export default props => (
+  <StaticQuery
+    query={indexQuery}
+    render={data => (
+      <AboutPage location={props.location} data={data} {...props} />
+    )}
+  />
+)
